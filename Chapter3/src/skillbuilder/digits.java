5107042eea32cb17ea982a63a7c5b80f9c611539
1100149017 <1100149017@L815WD220011.edu.cbe.ab.ca>
2026-09-14 10:11:36 -0600

    10:11, 9/14/26

--- Chapter3/src/skillbuilder/digits.java
+++ Chapter3/src/skillbuilder/digits.java
@@ -6,6 +6,24 @@ public class digits {
 
 	public static void main(String[] args) 
 	{
-		
+		//declaration
+		int i;
+		double d;
+		int x;
+
+		//User input creation
+		 Scanner userinput = new Scanner (System.in);
+		 
+		 //Get user input for int
+		 System.out.print("Enter integer: ");
+		 i = userinput.nextInt();
+		 
+		 //Get user input for double
+		 System.out.print("Enter double: ");
+		 d = userinput.nextInt();
+
+		x= i * (int)d ;
+		System.out.println
+		System.out.println("Your digits are: " + x);
 	}
 	}
