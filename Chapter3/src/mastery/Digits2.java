c6b3bee2efe076be2c87b0a917d3edf7b9730f6f
1100149017 <1100149017@L815WD220011.edu.cbe.ab.ca>
2026-09-17 11:19:47 -0600

    11:19AM, 9/17/26

--- Chapter3/src/mastery/Digits2.java
+++ Chapter3/src/mastery/Digits2.java
@@ -1,5 +1,34 @@
-package mastery;
+package skillbuilder;
 
-public class Digits2 {
+import java.util.Scanner;
 
+public class digits2 {
+
+    public static void main(String[] args) {
+
+        // declaration
+        double userNumber;
+        int number;  int tens; int ones;
+
+        // User input creation
+        Scanner userinput = new Scanner (System.in);
+        
+        System.out.print("Enter a two digit number:");
+        
+        // Get user input as a double
+        userNumber = userinput.nextDouble();
+
+        //double -> int 
+        number = (int) userNumber;
+
+        // Extract digits
+        tens = number / 10;
+        ones = number % 10;
+
+        System.out.println("Tens digit: " + tens);
+        System.out.println("Ones digit: " + ones);
+        
+        
+        // Jack Nguyen
+    }
 }
