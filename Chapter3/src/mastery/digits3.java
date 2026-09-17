8c42c2a7529efaef4ed0f2a122306a6225831cf1
1100149017 <1100149017@L815WD220011.edu.cbe.ab.ca>
2026-09-17 11:52:18 -0600

    11:52AM, 9/17/26

--- Chapter3/src/mastery/digits3.java
+++ Chapter3/src/mastery/digits3.java
@@ -2,18 +2,18 @@ package mastery;
 
 import java.util.Scanner;
 
-public class Digits3 {
+public class digits3 {
 
     public static void main(String[] args) {
 
         // declaration
         double userNumber;
-        int number;  int tens; int ones;
+        int number;  int hundreds; int tens; int ones;
 
         // User input creation
         Scanner userinput = new Scanner (System.in);
         
-        System.out.print("Enter a two digit number:");
+        System.out.print("Enter a three digit number:");
         
         // Get user input as a double
         userNumber = userinput.nextDouble();
@@ -22,9 +22,11 @@ public class Digits3 {
         number = (int) userNumber;
 
         // Extract digits
-        tens = number / 10;
+        hundreds = number / 100;
+        tens = (number / 10) % 10;
         ones = number % 10;
 
+        System.out.println("Hundreds digit: " + hundreds);
         System.out.println("Tens digit: " + tens);
         System.out.println("Ones digit: " + ones);
         
