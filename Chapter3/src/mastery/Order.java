a84c2a4c9a3c7dace0647c5254f3235bf387ce74
1100149017 <1100149017@L815WD220011.edu.cbe.ab.ca>
2026-09-22 11:31:55 -0600

    11:31AM

--- Chapter3/src/mastery/Order.java
+++ Chapter3/src/mastery/Order.java
@@ -11,30 +11,34 @@ public class Order {
     	double Burgers = 1.69;
     	double fries = 1.09;
     	double sodas = 0.99;
-    	double tax = 0.65; // 6.5% tax
-    	double price = 100.0;
-    	double Totaltax = price * tax;
+    	double tax = 0.065; // 6.5% tax
     	
     	//user input
     	Scanner userinput = new Scanner(System.in);
-    	userOrder = userinput.nextDouble();
     	
     	System.out.println("Each burger is $1.69");
     	System.out.println("Each portion of fies is $1.09");
     	System.out.println("Each cup of Soda is $0.99"); 
     	
     	System.out.print("Enter an amount of burgers you'd like to order:");
+    	userOrder = userinput.nextDouble();
+    	System.out.println("Enter an amount of fries you'd like to order:");
+    	userOrder = userinput.nextDouble();
+    	System.out.println("Enter an amount of sodas you'd like to order:");
+    	userOrder = userinput.nextDouble();
     			
+    
+
+    			
+    	double Totaltax = userOrder * tax;
         
     	
+    	System.out.println("Your total before tax:" + userOrder);
+    	System.out.print("Tax:" + Totaltax);
+    	System.out.print("Your final cost is:" + Totaltax + userOrder);
     }
 }
-    	
-    	
-    
-    
-
-    
 
 
-                                                                                                                                  
+// Jack Nguyen
+    	                                                                                                                     
