9e549f37123d97f80fdba86b72eae75a72e1a0ee
1100149017 <1100149017@L815WD220011.edu.cbe.ab.ca>
2026-09-22 10:53:44 -0600

    10:53AM

--- Chapter3/src/mastery/Order.java
+++ Chapter3/src/mastery/Order.java
@@ -1,5 +1,19 @@
 package mastery;
+import java.util.Scanner;
 
 public class Order {
+	
+    public static void main(String[] args) {
+    	
+    	
+    }
+    	
+    	//declaration
+    	double userOrder;
+    	int burgers; int fries; int sodas;
+    	burgers = $1.69 
+    }
+    
+    {
 
 }
