867f718874cb28302ccb4a1775c49f6bd4d76155
1100149017 <1100149017@L815WD220011.edu.cbe.ab.ca>
2026-09-24 11:53:05 -0600

    11:52AM

--- Chapter4/src/skillbuilder/Hurricane.java
+++ Chapter4/src/skillbuilder/Hurricane.java
@@ -4,10 +4,23 @@ import java.util.Scanner;
 
 public class Hurricane {
     public static void main(String[] args) {
-    	Scanner userInput = new Scanner (System.in);
+    	Scanner userinput = new Scanner (System.in);
     	
     	//declaration
+    	int category;
     	
+    	//Get input from user
+    	System.out.print("Enter the Saffir-Simpson hurrican category from 1-5: ");
+    	category = userinput.nextInt();
+    	
+    	// checking category
+    	if (category ==1 ) {
+    		System.out.print("Category 1 Hurrycane");
+    		System.out.print("MPH: 74-95");
+    		System.out.print("61-82 kts");
+    		System.out.print("119-153 km/hr");
+    		
+    	}
 
 
 }
